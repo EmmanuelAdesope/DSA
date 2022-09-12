922024cdd30447aaa698c3dd9bf9c406247891b4
Emmanuel Adesope <emmanueladesope@gmail.com>
2022-09-12 22:08:33 +0100

    SearchTree.java modified

---
+++ src/com/Algorithms/SearchTree.java
@@ -0,0 +1,21 @@
+package com.Algorithms;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+This program searches for a node at any depth p in a time in the order of P
+ */
+public class SearchTree {
+    public static void main(String[] args) {
+
+        // The class is used to create a queue
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // the root node is added as first element.
+        deque.add(0);
+
+
+
+    }
+}
