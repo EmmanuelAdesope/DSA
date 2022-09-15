cff3253cd182c8625c5bd112ce050ee1b1d265f6
Emmanuel Adesope <emmanueladesope@gmail.com>
2022-09-15 21:52:28 +0100

    SearchTree.java modified

--- src/com/Algorithms/SearchTree.java
+++ src/com/Algorithms/SearchTree.java
@@ -1,7 +1,9 @@
 package com.Algorithms;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 /*
 This program searches for a node at any depth p in a time in the order of P
@@ -28,5 +30,22 @@ public Edge(int source, int dest){
     this.source = source ;
     this.dest = dest ;
     }
+
+ }
+
+ class Graph {
+
+    ArrayList<ArrayList<Integer>>  adjList  = new ArrayList<>();
+
+    /*The graph Constructor. n is the number of nodes or vertices.
+     it constructs the graph from the list of edges.
+     */
+  Graph(List<Edge> edges, int n)
+  {
+      for (int i = 0; i < n; i++){
+
+      adjList.add(new ArrayList<>());
+      }
+  }
 
  }
