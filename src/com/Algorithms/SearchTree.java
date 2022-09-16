9a84fea6f8d424f4b3f5a6bc4214645cd5dc587e
Emmanuel Adesope <emmanueladesope@gmail.com>
2022-09-16 22:03:44 +0100

    SearchTree.java modified

--- src/com/Algorithms/SearchTree.java
+++ src/com/Algorithms/SearchTree.java
@@ -1,25 +1,81 @@
 package com.Algorithms;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /*
 This program searches for a node at any depth p in a time in the order of P
  */
 public class SearchTree {
+
     public static void main(String[] args) {
 
-        // The class is used to create a queue
+            // List of graph edges as per the above diagram
+            List<Edge> edges = Arrays.asList(
+                    new Edge(1, 2), new Edge(1, 3), new Edge(1, 4), new Edge(2, 5),
+                    new Edge(2, 6), new Edge(5, 9), new Edge(5, 10), new Edge(4, 7),
+                    new Edge(4, 8), new Edge(7, 11), new Edge(7, 12)
+                    // vertex 0, 13, and 14 are single nodes
+            );
+
+            // total number of nodes in the graph (labelled from 0 to 14)
+            int n = 15;
+
+            // build a graph from the given edges
+            Graph graph = new Graph(edges, n);
+
+            // to keep track of whether a vertex is discovered or not
+            boolean[] discovered = new boolean[n];
+
+            // Perform BFS traversal from all undiscovered nodes to
+            // cover all connected components of a graph
+            for (int i = 0; i < n; i++)
+            {
+                if (!discovered[i])
+                {
+                    // start BFS traversal from vertex `i`
+                    BFS(graph, i, discovered,7);
+                }
+            }
+        }
+
+
+
+    // Perform BFS on the graph starting from vertex `v`
+    public static void BFS(Graph graph, int v, boolean[] discovered, int key) {
+        // create a queue for doing BFS
         Deque<Integer> deque = new ArrayDeque<>();
 
-        // the root node is added as first element.
-        deque.add(0);
 
+        // mark the source vertex as discovered
+        discovered[v] = true;
+
+        // enqueue source vertex
+        deque.add(v);
+
+        // loop till queue is empty
+        while (!deque.isEmpty()) {
+            // dequeue front node and print it
+            v = deque.poll();
+            System.out.println( v + " ");
+
+            // do for every edge (v, u)
+            for (int u : graph.adjList.get(v)) {
+                if (u == key) {
+                    System.out.println(key + "is present");
+                    break;
+                }
+
+                if (!discovered[u]) {
+                    // mark it as discovered and enqueue it
+                    discovered[u] = true;
+                    deque.add(u);
+                }
+            }
+        }
 
 
     }
+
 }
 
 // Class to store a graph edge
@@ -46,6 +102,15 @@ public Edge(int source, int dest){
 
       adjList.add(new ArrayList<>());
       }
+      for (Edge edge: edges )
+      {
+          int src = edge.source;
+          int dest = edge.dest;
+
+          adjList.get(src).add(dest);
+          adjList.get(dest).add(src);
+      }
+
   }
 
  }
