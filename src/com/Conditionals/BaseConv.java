9f5fd0904129b01d690b028073c7c82aec4b6f87
Emmanuel Adesope <emmanueladesope@gmail.com>
2022-09-23 21:58:20 +0100

    BaseConv.java modified

--- src/com/Conditionals/BaseConv.java
+++ src/com/Conditionals/BaseConv.java
@@ -21,16 +21,16 @@ public class BaseConv {
             {
                 long m ;
                 long n  ;
-                String base = " ";
+                StringBuilder base = new StringBuilder(" ");
                 while (i/k !=0)
                 {  m = i/k;
                     n = i%k;
-                    base = n + base;
+                    base.insert(0, n);
                     System.out.println("Trace "  + m +"  " +  n + "  "+ base);
                     if( m < k)
                     { long a = m/k ;
                         n = m - (a*k);
-                        base = n + base ;
+                        base.insert(0, n);
                         System.out.println("Trace "  + a +"  " +  n + "  "+base);
                     }
 
@@ -41,44 +41,45 @@ public class BaseConv {
             {
                 long m ;
                 long n ;
-                String base = " ";
+                StringBuilder base = new StringBuilder(" ");
                 while (i/k !=0)
                 {  m = i/k;
                     n = i%k;
                     // base = n + base;
                     //System.out.println("Trace "  + m +"  " +  n + "  "+ base);
                     if (n==10)
-                    {base = "A" + base;
+                    {
+                        base.insert(0, "A");
                         System.out.println("Trace " + m +" " + n + " "+ base);
                     }
                     else if(n==11)
-                    {  base = "B" + base;
+                    {  base.insert(0, "B");
                         System.out.println("Trace" + m +" " + n + " "+ base);
                     }
                     else if(n==12)
-                    { base = "C" + base;
+                    { base.insert(0, "C");
                         System.out.println("Trace" + m +" " + n + " "+ base);
                     }
                     else if(n==13)
-                    { base = "D" + base;
+                    { base.insert(0, "D");
                         System.out.println("Trace" + m +" " + n + " "+ base);
                     }
                     else if(n==14)
-                    { base = "E" + base;
+                    { base.insert(0, "E");
                         System.out.println("Trace" + m +" " + n + " "+ base);
                     }
                     else if (n==15)
-                    { base = "F" + base;
+                    { base.insert(0, "F");
                         System.out.println("Trace" + m +" " + n + " "+ base);
                     }
                     else if (n<10)
-                    { base = n + base;
+                    { base.insert(0, n);
                     }System.out.println("Trace "  + m +"  " +  n + "  "+ base);
 
                     if( m < k)
                     { long a = m/k ;
                         n = m - (a*k);
-                        base = n + base ;
+                        base.insert(0, n);
                         System.out.println("Trace "  + a +"  " +  n + "  "+base);
                     }
                     i = m;
