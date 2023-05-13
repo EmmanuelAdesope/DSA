5c9a0aa2fca5132cefcab3dbe725067d675893a4
Emmanuel Adesope <emmanueladesope@gmail.com>
2023-05-13 12:41:24 +0100

    Spreadsheet.java added

--- src/com/Arrays/Spreadsheet.java
+++ src/com/Arrays/Spreadsheet.java
@@ -7,9 +7,9 @@ public class Spreadsheet {
 
     public static void main(String[] args) {
 
-
+        // Spreadsheet scores
         int [][] a = {{99,85,98,0,0},{98,57,78,0,0},{92,77,76,0,0},{94,32,11,0,0},{99,34,22,0,0},{90,46,54,0,0},{76,59,88,0,0},{92,66,89,0,0},{97,71,24,0,0},{89,29,38,0,0}};
-        int [] b = new int[a.length];
+      // weights
         double[] weights = {0.4, 0.3, 0.3, 0, 0};
 
 
